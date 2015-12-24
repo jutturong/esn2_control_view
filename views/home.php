6e74274d6dff6b59f465235ab7416a791a0cd27f
jutturong <jutturongcharoenrit@gmail.com>
2015-12-24 13:51:40 +0700

    24-12-58 file1

--- views/home.php
+++ views/home.php
@@ -3,7 +3,7 @@
     <head>
         
         
-        
+        <!-- test git form macbook -->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><?=$title?></title>
         
